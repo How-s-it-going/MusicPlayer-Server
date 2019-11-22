0d9124e9a1bf16f8b811fed7b172b62b557e8a91
spea <spea55sugaku@outlook.jp>
2019-11-22 22:08:31 +0900

    initial commit

--- main/Server.go
+++ main/Server.go
@@ -4,44 +4,116 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"time"
+	"os/exec"
+	"strings"
 )
+
+var musicCh = make(chan string)
+var links = "C:/Users/626ca/IdeaProjects/SocketChat/src/spea55/links.txt"
 
 func main() {
 
 	port := ":28923"
-	Addr, err := net.ResolveTCPAddr("tcp", port)
-	checkError(err)
-	listener, err := net.ListenTCP("192.168.11.3", Addr)
-	checkError(err)
+	Addr, err1 := net.ResolveTCPAddr("tcp", port)
+	if err1 != nil {
+		fmt.Println("Dial error5:", err1)
+	}
+	listener, err := net.ListenTCP("tcp", Addr)
+	if err != nil {
+		fmt.Println("Dial error1:", err)
+	}
+	fmt.Println("クライアントからの入力待ち")
+	go PlayMusicLoop()
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			continue
 		}
-
-		go handleClient(conn)
+		go ListenClient(conn)
 	}
 }
 
-func handleClient(conn net.Conn) {
+func ListenClient(conn net.Conn) {
 	defer conn.Close()
-	_ = conn.SetReadDeadline(time.Now().Add(10 * time.Second))
-	fmt.Println("Client accept")
+	var line string
 	messageBuf := make([]byte, 1024)
 	messageLen, err := conn.Read(messageBuf)
-	checkError(err)
+	if err != nil {
+		fmt.Println("Dial error2:", err)
+	}
+	for {
+		line = string(messageLen)
+		if line == "/bye" {
+			break
+		}
 
-	message := string(messageBuf[:messageLen])
-	message = message + " too!"
+		if strings.HasPrefix(line, "/play") {
+			var url = strings.Split(line, " ")[1]
+			fmt.Println(url)
 
-	_ = conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
-	_, _ = conn.Write([]byte(message))
+			message := string(messageBuf[:messageLen])
+
+			_, _ = conn.Write([]byte(message))
+
+			path := getPath(url)
+			if &path != nil {
+				/*if musicCh <-strings.SplitAfter(path,"\\.")[0]+".mp3" {
+					_, _ = conn.Write([]byte("Added of Queue."))
+				}else {
+					_, _ = conn.Write([]byte("Queue is full."))
+				} */
+			} else {
+				var p = exec.Command("python " + "C:/Users/626ca/PycharmProjects/tubedoeloader/download.py " + url)
+				_ = p.Wait()
+
+				path = getPath(url)
+				cmd := "ffmpeg -i \"" + path + "\" \"" + strings.Split(path, "\\.")[0] + ".mp3\""
+				p2 := exec.Command(cmd)
+				_ = p2.Wait()
+				/*paths := strings.Split(path,"\\.")[0]+".mp3"
+				if musicCh <- paths {
+					_, _ = conn.Write([]byte("Added of Queue."))
+				}else {
+					_, _ = conn.Write([]byte("Queue is full."))
+				} */
+			}
+		}
+	}
 }
 
-func checkError(err error) {
-	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "fatal: %s", err.Error)
-		os.Exit(1)
+func getPath(url string) (str string) {
+	var line, err1 = os.Open(links)
+	if err1 != nil {
+		fmt.Println("Dial error3:", err1)
 	}
+	var lines, err2 = line.Read([]byte(links))
+	if err2 != nil {
+		fmt.Println("Dial error4:", err2)
+	}
+	var path string
+	defer line.Close()
+	for {
+		tmp := strings.Split(string(lines), "&,")[0]
+		if tmp == url {
+			fmt.Println("hit.")
+			path = strings.Split(links, ",")[1]
+			path = "C:/Users/626ca/IdeaProjects/SocketChat/src/spea55/" + path
+			return path
+		}
+	}
+	return path
+}
+
+func PlayMusicLoop() {
+	for {
+		path := <-musicCh
+		_, _ = os.OpenFile(links, os.O_RDWR|os.O_CREATE, 0666)
+		PlayMusicPyWrapper(strings.Split(path, "\\.")[0] + ".mp3")
+	}
+}
+
+//PlayMusicPyWrapper
+func PlayMusicPyWrapper(path string) {
+	p := exec.Command("python C:/Users/626ca/PycharmProjects/music_player/play_music.py " + path)
+	_ = p.Wait()
 }
